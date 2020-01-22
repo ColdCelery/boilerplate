439fd4d44132a4cac7437ab21566d2b8b0b8a938
Kyle Culleton <kecn88@gmail.com>
2020-01-22 03:50:11 -0500

    init commit

--- src/app.js
+++ src/app.js
@@ -3,10 +3,11 @@ const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
 const helmet = require("helmet");
+const { NODE_ENV } = require("./config");
 
 const app = express();
 
-const morganOptions = process.env.NODE_ENV === "production" ? "tiny" : "common";
+const morganOption = NODE_ENV === "production";
 app.use(morgan(morganOptions));
 app.use(helmet());
 app.use(cors());
@@ -15,7 +16,7 @@ app.get("/", (req, res) => {
 });
 app.use(function errorHandler(error, req, res, next) {
   let response;
-  if (process.env.NODE_ENV === "production") {
+  if (NODE_ENV === "production") {
     response = { error: { message: "server error" } };
   } else {
     console.log(error);
